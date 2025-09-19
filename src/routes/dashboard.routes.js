72ef603ad426d764e2a8100c9da6433fe9d7994c
Karden-dev <kouemobardon@gmail.com>
2025-09-19 13:57:23 +0100

    Delete src directory

--- src/routes/dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/dashboard.routes.js
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboard.controller');
-const authMiddleware = require('../middleware/auth.middleware');
-
-router.get('/stats', authMiddleware.verifyToken, authMiddleware.isAdmin, dashboardController.getDashboardStats);
-
-module.exports = router;
